824f5bd3d3eefad055a43cc920172ccba4a2d144
JohDan <chinedunomeh2015@gmail.com>
2024-07-30 11:05:34 +0100

    Task 2 commit

--- JOHN_DANIEL/part_2/task_2.py
+++ JOHN_DANIEL/part_2/task_2.py
@@ -5,3 +5,33 @@
 #Output result
 
 """Execution"""
+digits = []
+letters = []
+sentence = []
+dig_letters = 0
+dig_number = 0
+
+alphabets = ["a","A",'b','B', 'c','C', 'd','D','e',"E", 'f','F','g', 'G', 'h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','w','W','x','X','y','Y','z', 'Z']
+
+numbers = ['1','2','3','4','5','6','7','8','9','0']
+
+
+user_input = input("Enter sentence: ")
+sentence += user_input
+
+for char in sentence:
+	for index in numbers:
+		if char == index:
+			digits += [index]
+			dig_number += 1
+	for character in alphabets:
+		if char == character:
+			letters += [character]
+			dig_letters += 1
+
+print(f"LETTERS: {letters}   DIGITS: {digits}")
+print(f"LETTERS: {dig_letters}   DIGITS: {dig_number}")
+
+#print(sentence)
+		
+	
