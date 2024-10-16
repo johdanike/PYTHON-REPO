8516f021dd99790be9ad914599ec606d9f6541f1
JohDan <chinedunomeh2015@gmail.com>
2024-10-16 20:50:11 +0100

    Another commit

--- class-work/October_2024/dsa/stack/stack.py
+++ class-work/October_2024/dsa/stack/stack.py
@@ -1,10 +1,9 @@
-from os import remove
 
 
 class Stack:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.data = [] * capacity
+        self.data = [None] * capacity
         self.number = 0
         self.is_empty = True
 
@@ -17,11 +16,6 @@ class Stack:
     def get_is_empty(self):
         return self.is_empty
 
-    # def push(self, param):
-    #     # self.is_empty = False
-    #     self.data.append(param)
-    #     self.size += 1
-
     def push(self, param):
         if self.is_full():
             raise ValueError("Stack is full")
@@ -29,11 +23,13 @@ class Stack:
         self.number += 1
 
     def pop(self):
-        for i in range(len(self.data)):
-            remove(str(self.data[i-1]))
+        for index in range(len(self.data)):
+            str(self.data[index-1])
+            self.number -= 1
         return self.data
 
-
+    def get_number(self):
+        return self.number
 
     def is_full(self):
         return self.number == self.capacity
--- class-work/October_2024/dsa/stack/test_stack.py
+++ class-work/October_2024/dsa/stack/test_stack.py
@@ -3,7 +3,7 @@ from dsa.stack.stack import Stack
 
 class TestStack(unittest.TestCase):
     def test_that_i_can_push_X_stack_is_not_empty(self):
-        my_stack = Stack()
+        my_stack = Stack(3)
         my_stack.push("The Good news")
         when = my_stack.stack_is_not_empty()
         self.assertTrue(True, when)
@@ -14,4 +14,46 @@ class TestStack(unittest.TestCase):
         my_stack.push("The Good news")
         self.assertTrue(True, my_stack.stack_is_not_empty())
         my_stack.pop()
-        self.assertTrue(False, my_stack.stack_is_empty())
+        self.assertFalse(False, my_stack.stack_is_empty())
+
+    def test_that_i_can_push_X_push_Y_pop_X_stack_is_not_empty(self):
+        my_stack = Stack(3)
+        self.assertTrue(True, my_stack.stack_is_empty())
+        my_stack.push("The Good news")
+        self.assertTrue(True, my_stack.stack_is_empty())
+        my_stack.push("The Good shepherd")
+        self.assertTrue(True, my_stack.stack_is_empty())
+        my_stack.pop()
+        self.assertFalse(False, my_stack.stack_is_empty())
+
+    def test_that_i_can_push_XYZ_pop_Z_stack_is_not_empty(self):
+        my_stack = Stack(3)
+        self.assertTrue(True, my_stack.stack_is_empty())
+        my_stack.push("The Good news")
+        my_stack.push("The Good shepherd")
+        my_stack.push("The Good Lamb")
+        my_stack.pop()
+        self.assertFalse(False, my_stack.stack_is_empty())
+
+    def test_that_i_can_push_XYZ_stack_is_full(self):
+        my_stack = Stack(3)
+        my_stack.push("The Good news")
+        my_stack.push("The Good shepherd")
+        my_stack.push("The Good Lamb")
+        self.assertTrue(True, my_stack.stack_is_not_empty())
+
+    # def test_that_i_stack_will_throw_exception_if_full(self):
+    #     my_stack = Stack(3)
+    #     my_stack.push("The Good news")
+    #     my_stack.push("The Good shepherd")
+    #     my_stack.push("The Good Lamb")
+    #     my_stack.push("the good human")
+    #     # self.assertRaises(ValueError, my_stack.is_full())
+    #     self.assertRaises(ValueError, my_stack.push, "the good human")
+
+    def test_push_X_pop_X_stack_is_zero(self):
+        my_stack = Stack(3)
+        my_stack.push("The Good news")
+        self.assertFalse(my_stack.stack_is_empty())
+        my_stack.pop()
+        self.assertTrue(my_stack.stack_is_empty())
